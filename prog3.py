ee0a825dc84f33da25c4d1708b34abceea9b5bb4
Kezia Ardia Sasaki <keziaardia03@gmail.com>
2024-11-08 09:33:57 +0800

    Add files via upload

---
+++ prog3.py
@@ -0,0 +1,25 @@
+#variabel assigment value
+x = 14
+y  = 7
+print("nilai dari y adalah : ", y)
+
+# aturan penamaan variabel
+luas = 60 # bisa berupa kata
+chenle_ganteng = 100 # jika dua kata,digabung dengan "_"
+Keziax = 21 # boleh digabung capital non-capital
+keziax = 27
+lenca88 = 20 # jika menggabungkan huruf dan angka, angkanya di akhir
+print("nilai dari kezia adalah : ", lenca88)
+
+
+a = 6 # integer bilangan bulat
+print("tipe data dari a : ", type (a))
+
+b = 9,8 # float bilangan pecahan
+print("tipe data dari b : ", type (b))
+
+c = "keren" # string, gabungan karakter
+print("tipe data dari c : ", type (c))
+
+d = False # Boolean, true false
+print("tipe data dari d : ", type (d) )
