3e7b3f93ec22370acc6616f73d4f35b65a5c3b8a
Kezia Ardia Sasaki <keziaardia03@gmail.com>
2024-11-15 09:29:49 +0800

    Add files via upload

---
+++ prog4.py
@@ -0,0 +1,9 @@
+# modifikasi program 5 agar mengandung masukan user
+
+a = int(input ("masukan alas"))
+print ("nilai dari a adalah : ", a, type (a))
+
+b = int(input ("masukan tinggi"))
+print ("nilai dari b adalah : ", b, type (b))
+hasil = a*b 
+print ("hasil dari a*b adalah : ", hasil)
