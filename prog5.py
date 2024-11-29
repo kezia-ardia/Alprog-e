8ee4e7bc2a17b662fd60ff0d70ab0c8a9da768f9
Kezia Ardia Sasaki <keziaardia03@gmail.com>
2024-11-29 09:37:51 +0800

    Add files via upload

---
+++ prog5.py
@@ -0,0 +1,34 @@
+x = 24
+y = 69
+
+# operasi penjumlahan
+hasil = x + y
+print ("nilai dari x + y = ", hasil)
+
+# pengurangan
+hasil = x - y
+print ("nilai dari x - y = ", hasil)
+
+# pembagian
+hasil = x / y
+print ("nilai dari x / y = ", hasil)
+
+# perkalian
+hasil = x * y
+print ("nilai dari x * y = ", hasil)
+
+# eksponen (perpangkatan)
+hasil = x ** y
+print ("nilai dari x ** y = ", hasil)
+
+# modulus
+hasil = x // y
+print ("nilai dari x // y = ", hasil)
+
+# floor division
+hasil = x // y
+print ("nilai dari x FD y = ", hasil)
+
+# operasi prioritas
+# 1. kerjakan dari kiri ke kanan 
+# 2. perpangkatan perkalian/pembagian pengurangan/penambahan
